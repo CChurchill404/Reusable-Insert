e5b6b2350bda9e6e3db4f83c3a924f4c11af6f1c
Connor_Churchill <32199784+CChurchill404@users.noreply.github.com>
2019-10-14 16:59:52 +0100

    Update user.php

--- user.php
+++ user.php
@@ -1,44 +1,66 @@
 <?php
-include "dbcon.php";
- 
-  class dataOperations extends Dbh
-  {
-    //insert method 
-        public function insert_data($table,$fields){
-            $insertUserDataSQL = "";
-            $insertUserDataSQL .="INSERT INTO ".$table;
-            $insertUserDataSQL .=" (".implode(",", array_keys($fields)).") VALUES ";
-            $insertUserDataSQL .="('".implode("','", array_values($fields))."')";
-            $query = mysqli_query($this->conn,$insertUserDataSQL);
-            if($query){
-              return true;
-            }
+include "connectionClass.php";
+
+class dataOperations extends Dbh
+{
+    public function insert_data($table, $fields)
+    {
+        $insertDataSQL = "";
+        $insertDataSQL .= "INSERT INTO " . $table;
+        $insertDataSQL .= " (" . implode(",", array_keys($fields)) . ") VALUES ";
+        $insertDataSQL .= "('" . implode("','", array_values($fields)) . "')";
+        echo $insertDataSQL;
+        $query = mysqli_query($this->conn, $insertDataSQL);
+        if ($query) {
+            return true;
         }
-    //select method
-      public function select_data($table){
-            $selectDataSQL = "SELECT * FROM" . $table;
-            $array = array();
-            $query = mysqli_query($this->conn, $selectDataSQL);
-            while ($row = mysql_fetch_assoc($query)) {
-                $array[] = $row;
-            }
-            return $array;
-      }
-  }
-
-  
-
-//using the insert method
-    $insertUserData = new dataOperations;
-    if(isset($_POST["submit"])){
-      $myArray = array(
-        "userName"=> $_POST["userName"],
-        "passWord"=> $_POST["Pass"]
-      );
-
-    if($insertUserData->insert_data("userdetails",$myArray)){
-      header("location:index.php?msg=Record Inserted");
     }
-  }
 
+    public function fetch_data($table)
+    {
+        $fetchDataSQL = "SELECT * FROM" . $table;
+        $array = array();
+        $query = mysqli_query($this->conn, $fetchDataSQL);
+        while ($row = mysql_fetch_assoc($query)) {
+            $array[] = $row;
+        }
+        return $array;
+    }
+
+    public function select_data($table, $objectID)
+    {
+        $selectDataSQL = "";
+        $sqlCondition = "";
+        foreach ($objectID as $age => $value) {
+            $sqlCondition = $age . "='" . $value . "' AND ";
+        }
+        $sqlCondition = substr($sqlCondition, 0, -5);
+        $selectDataSQL .= "SELECT * FROM " . $table . " WHERE " . $sqlCondition;
+        $query = mysqli_query($this->conn, $selectDataSQL);
+        $row = mysqli_fetch_array($query);
+        return $row;
+    }
+
+    public function update_data($table, $objectID, $fields){
+        $updateDataSQL = "";
+        $sqlUpdateCondition = "";
+        foreach ($objectID as $key => $value) {
+            $sqlUpdateCondition = $key . "='" . $value . "' AND ";
+        }
+        $sqlUpdateCondition = substr($sqlUpdateCondition, 0, -5);
+        //updating a name
+        foreach ($fields as $key => $value) {
+            $updateDataSQL .= $key . "='".$value."', ";
+        }
+        $updateDataSQL = substr($updateDataSQL, 0,-2);
+        $updateDataSQL = "UPDATE ".$table." SET ".$updateDataSQL." WHERE ".$sqlUpdateCondition;
+        if (mysqli_query($this->conn,$updateDataSQL)) {
+            return true;
+        }
+    }
+
+    public function delete_data(){
+
+    }
+}
 ?>
